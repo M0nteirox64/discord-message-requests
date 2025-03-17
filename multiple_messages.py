355c7a85a6751ebab0f57fbc6bd7366d77b009aa
MonteiroPSV <trxshroot@gmail.com>
2025-03-17 21:30:10 +0000

    Update multiple_messages.py

--- multiple_messages.py
+++ multiple_messages.py
@@ -4,7 +4,7 @@ url = "https://discord.com/api/v9/channels/1351297427285741693/messages"
 # https://discord.com/api/v9/channels/CHANNEL_ID/message
 
 payload = {
-    "content": "teste" # Message
+    "content": "Message" # Message
 }
 
 headers = {
