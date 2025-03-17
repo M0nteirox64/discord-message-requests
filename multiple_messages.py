8e3a72a46d4e9a45a576ecc155dfc8cd8e79b876
MonteiroPSV <trxshroot@gmail.com>
2025-03-17 21:27:58 +0000

    Create multiple_messages.py

---
+++ multiple_messages.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "https://discord.com/api/v9/channels/1351297427285741693/messages"
+# https://discord.com/api/v9/channels/CHANNEL_ID/message
+
+payload = {
+    "content": "teste" # Message
+}
+
+headers = {
+    "Authorization": "---" # Accout token
+}
+
+for i in range(5):  
+    requests.post(url, payload, headers=headers)
